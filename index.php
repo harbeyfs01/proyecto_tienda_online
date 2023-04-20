4341721a38b077fb34b593493d20b1b384ded91b
harbeyfs <harbeyfs.01@gmil.com>
2023-04-19 21:13:36 -0500

    se agregan 2 archivos al proyecto

--- index.php
+++ index.php
@@ -20,7 +20,13 @@ $resultado = $sql->fetchAll(PDO::FETCH_ASSOC);
     <title> Tienda Online </title>
 
     <!-- Bootstrap CSS -->
-    <link href="url del bootstrap" rel=
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" 
+    rel="stylesheet" 
+    integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" 
+    crossorigin="anonymous">
+
+    
+    integrity="sha384-18mE4kWBq78iYhFldvKuhfTaU
     <link href=css/estilos.css rel="stylesheet">
 </head>
 
@@ -32,14 +38,14 @@ $resultado = $sql->fetchAll(PDO::FETCH_ASSOC);
                 <a href="#" class="navbar-brand">
                     <strong> Tienda Online </strong>
                 </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-base-target="#navbarHeader.....
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-base-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"> </span>
                 </button>
 
                 <div class="collapse navbar-collapse" id="navbarHeader">
                     <u1 class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item">
-                            <a href="#" class ="nav-link active"> catalogo </a>
+                            <a href="#" class ="nav-link active"> Catalogo </a>
                         </li>
                         <li class="nav-item">
                             <a href="#" class="nav-link"> Contacto </a>
@@ -85,6 +91,9 @@ $resultado = $sql->fetchAll(PDO::FETCH_ASSOC);
             </div>
         </div>
     </main>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" 
+    integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" 
+    crossorigin="anonymous"></script>
 
 </body>
 
